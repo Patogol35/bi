dc21e2c8fcad2c649c4ac4dcf0b33ac033ee3664
Patogol35 <patricio_jorge@hotmail.es>
2026-01-19 18:22:58 -0500

    back

--- config/settings.py
+++ config/settings.py
@@ -57,13 +57,18 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+import os
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "patogol3535@gmail.com"
-EMAIL_HOST_PASSWORD = "aptm figw jeqf zjmd"
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 ROOT_URLCONF = 'config.urls'
 
